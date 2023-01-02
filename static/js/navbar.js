78321e74f224addb3ff53d18c014bb79b90e8ba7
riikoovy <olli-pekka.v.riikola@student.jyu.fi>
2023-01-02 21:44:28 +0100

    commented js file also

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,42 +1,27 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropdownEffect(e) {
-    console.log('Drop');
-   // document.getElementById("dropdown").classList.toggle("show");
-}
 
+/* Add event listeners for all of the <a> elements
+   which has class .dropbtn */
 window.onload = () => {
     console.log('Window onload');
     var btns = document.getElementsByClassName('dropbtn');
     for (let element of btns){
 	element.addEventListener('click', dropItems);
-//	element.addEventListener('blur', hideItems);
+	//element.addEventListener('blur', hideItems);
     }
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onfocus = function(e) {
-    console.log('focus');  
-    if (e.target.className === 'dropbtn') {
-	var content = e.target.nextChild;
-	console.log(content);
-	if (content.classList.contains('show')) {
-	    content.classList.remove('show');
-	}
-    }
-}
-
+// Just toggle the .show class, if there appears to be dropdown content
 function dropItems(e){
-    // Close the dropdown if the user clicks outside of it
-    console.log(e.target);  
-    console.log(e.target.parentNode.children);
     let parent = e.target.parentNode;
     if (parent.children.length > 1){
 	parent.children[1].classList.toggle("show");
-	console.log(parent.children[1]);
     }
 }
 
+// TODO
+/* Not workin yet, needs to be added with listener above.*/
 function hideItems(e){
     // Close the dropdown if the user clicks outside of it
     console.log(e.target);  
@@ -47,3 +32,4 @@ function hideItems(e){
 	console.log(parent.children[1]);
     }
 }
+
